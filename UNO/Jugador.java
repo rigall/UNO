16f3eb8ac12b39d9b3037b92428d2bbd85d4de90
eloi rigall <erigall062@boscdelacoma.cat>
2025-03-11 12:33:04 +0100

    creacio classe Carta

---
+++ UNO/Jugador.java
@@ -0,0 +1,13 @@
+package UNO;
+
+class Jugador {
+    public String nom;
+
+    public Jugador(String nom) {
+        this.nom = nom;
+    }
+
+    public String getnom(){
+        return nom;
+    }
+}
