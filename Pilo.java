b7c1228c70f0ed4b6942c4b2a2b91645e550e1a3
eloi rigall <erigall062@boscdelacoma.cat>
2025-03-28 10:18:23 +0100

    Pilo, metodes consultarCarta

--- Pilo.java
+++ Pilo.java
@@ -1,24 +1,31 @@
-package UNO;
+package uno;
 
 import java.util.Stack;
 
 public class Pilo {
-    private Stack<Carta> pilo;
+    private Stack<Carta> cartes;
 
     public Pilo() {
-        pilo = new Stack<>();
+        cartes = new Stack<>();
     }
 
-    public Stack<Carta> getPilo() {
-        return pilo;
+    public Stack<Carta> getCartes() {
+        return cartes;
     }
 
     public void tirarCarta(Carta c) {
-        pilo.push(c);
+        cartes.push(c);
     }
 
-    public void buidarPilo(Carta ultimaCarta) {
-        pilo.clear();
-        pilo.push(ultimaCarta); // Deixa la ultima carta al pilo
+    public void addCarta(Carta carta) {
+        cartes.push(carta);
+    }
+
+    public Carta agafarCarta() {
+        return cartes.pop();
+    }
+
+    public Carta consultarCarta(){
+        return cartes.peek();
     }
 }
